5ef653dad54fdb28f3e38cebc28963c2a4a5778a
PaigeWing <paige_wing@aol.com>
2023-01-06 21:06:01 -0700

    added scripts to handlebars and put in blueprint javascripts for add buttons in profile.js

--- public/js/profle.js
+++ public/js/profle.js
@@ -1,47 +1,86 @@
-const newFormHandler = async (event) => {
+//Add New Allergy
+const addAllergy = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#project-name').value.trim();
-  const needed_funding = document.querySelector('#project-funding').value.trim();
-  const description = document.querySelector('#project-desc').value.trim();
+  const allergy = document.querySelector('#allergy-add').value.trim();
 
-  if (name && needed_funding && description) {
-    const response = await fetch(`/api/projects`, {
+  if (allergy) {
+    const response = await fetch('/api/allergies', {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      body: JSON.stringify({ allergy }),
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/allergies');
     } else {
-      alert('Failed to create project');
+      alert(response.statusText);
+    }
+  }
+};
+document.querySelector('.addButton').addEventListener('Submit', addAllergy);
+
+//Add New Condition
+const addCondition = async (event) => {
+  event.preventDefault();
+
+  const condition = document.querySelector('#condition-add').value.trim();
+
+  if (condition) {
+    const response = await fetch('/api/conditions', {
+      method: 'POST',
+      body: JSON.stringify({ condition }),
+    });
+
+    if (response.ok) {
+      document.location.replace('/conditions');
+    } else {
+      alert(response.statusText);
     }
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+document.querySelector('.addButton').addEventListener('Submit', addCondition);
 
-    const response = await fetch(`/api/projects/${id}`, {
-      method: 'DELETE',
+//Add New Drug
+const addDrug = async (event) => {
+  event.preventDefault();
+
+  const drug = document.querySelector('#drug-add').value.trim();
+
+  if (drug) {
+    const response = await fetch('/api/drugs', {
+      method: 'POST',
+      body: JSON.stringify({ drug }),
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace('/drugs');
     } else {
-      alert('Failed to delete project');
+      alert(response.statusText);
     }
   }
 };
 
-document
-  .querySelector('.new-project-form')
-  .addEventListener('submit', newFormHandler);
+document.querySelector('.addButton').addEventListener('Submit', addDrug);
 
-document
-  .querySelector('.project-list')
-  .addEventListener('click', delButtonHandler);
+//Add New Surgery
+const addSurgery = async (event) => {
+  event.preventDefault();
+
+  const surgery = document.querySelector('#surgery-add').value.trim();
+
+  if (surgery) {
+    const response = await fetch('/api/surgeries', {
+      method: 'POST',
+      body: JSON.stringify({ surgery }),
+    });
+
+    if (response.ok) {
+      document.location.replace('/surgeries');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document.querySelector('.addButton').addEventListener('Submit', addSurgery);
