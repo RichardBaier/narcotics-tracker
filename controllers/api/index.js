a0812b41aca8cd1df6adde33ff2677eb171ac8f2
SuperDork <bearbaier@gmail.com>
2023-01-07 12:44:57 -0500

    added the other routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 const patientRoutes = require('./patientRoutes');
 const conditionRoutes = require('./conditionRoutes');
+const surgeriesRoutes = require('./surgeriesRoutes');
+const allergiesRoutes = require('./allergiesRoutes');
+const drugsRoutes = require('./allergiesRoutes');
+
 
 router.use('/patients', patientRoutes);
 router.use('/conditions', conditionRoutes);
+router.use('/drugs', drugsRoutes);
+router.use('/surgeries', surgeriesRoutes);
+router.use('/allergies', allergiesRoutes);
 
 module.exports = router;
