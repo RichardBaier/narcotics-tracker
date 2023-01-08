06d6e094b720f8df75879db645c934a4dba12a12
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-01-08 12:03:26 -0700

    changes

--- src/dash.js
+++ src/dash.js
@@ -6,23 +6,23 @@ function openFormA() {
     document.getElementById("conditionsForm").style.display = "none";
   }
   function openFormB() {
-    document.getElementById("conditionsForm").style.display = "block";
+    document.getElementById("prescriptionsForm").style.display = "block";
   }
   
   function closeFormB() {
-    document.getElementById("conditionsForm").style.display = "none";
+    document.getElementById("prescriptionsForm").style.display = "none";
   }
   function openFormC() {
-    document.getElementById("conditionsForm").style.display = "block";
+    document.getElementById("allergiesForm").style.display = "block";
   }
   
   function closeFormC() {
-    document.getElementById("conditionsForm").style.display = "none";
+    document.getElementById("allergiesForm").style.display = "none";
   }
   function openFormD() {
-    document.getElementById("conditionsForm").style.display = "block";
+    document.getElementById("surgeriesForm").style.display = "block";
   }
   
   function closeFormD() {
-    document.getElementById("conditionsForm").style.display = "none";
+    document.getElementById("surgeriesForm").style.display = "none";
   }
