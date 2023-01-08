20f99824cf7a2848f3c18ec02166850412fb989d
Jeffrey Ackerman <ackerman.jeffrey@gmail.com>
2023-01-08 11:00:56 -0700

    Adding my front-end code which includes HTML and CSS and JS for the Sign-In Page, Create New Account Page and User Dashboard Page.

---
+++ src/dash.js
@@ -0,0 +1,28 @@
+function openFormA() {
+    document.getElementById("conditionsForm").style.display = "block";
+  }
+  
+  function closeFormA() {
+    document.getElementById("conditionsForm").style.display = "none";
+  }
+  function openFormB() {
+    document.getElementById("conditionsForm").style.display = "block";
+  }
+  
+  function closeFormB() {
+    document.getElementById("conditionsForm").style.display = "none";
+  }
+  function openFormC() {
+    document.getElementById("conditionsForm").style.display = "block";
+  }
+  
+  function closeFormC() {
+    document.getElementById("conditionsForm").style.display = "none";
+  }
+  function openFormD() {
+    document.getElementById("conditionsForm").style.display = "block";
+  }
+  
+  function closeFormD() {
+    document.getElementById("conditionsForm").style.display = "none";
+  }
