4271167a6ee85fb0d83367c85131c96c36b20689
RainbowToad <chad_spilker@hotmail.com>
2023-01-07 17:39:23 -0800

    fixed routing on index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,19 +1,16 @@
 const router = require('express').Router();
 const patientRoutes = require('./patientRoutes');
 const conditionRoutes = require('./conditionRoutes');
-const surgeriesRoutes = require('./surgeriesRoutes');
-const allergiesRoutes = require('./allergiesRoutes');
-const drugsRoutes = require('./allergiesRoutes');
+const surgeriesRoutes = require('./surgeries');
+const allergiesRoutes = require('./allergies');
+const drugsRoutes = require('./drugs');
 
 
 router.use('/patients', patientRoutes);
 router.use('/conditions', conditionRoutes);
-<<<<<<< HEAD
 router.use('/drugs', drugsRoutes);
 router.use('/surgeries', surgeriesRoutes);
 router.use('/allergies', allergiesRoutes);
-=======
-// Do we need to add additional routes here??
->>>>>>> 5ce3bd8392dac372b961dbcdfe9a6b76f71d8327
+
 
 module.exports = router;
